fd67ebe046b27362bd4a87d1cb6b1ae7d32c8054
Rish Saddiq <rishab.saddiq0607@gmail.com>
2024-05-22 22:25:15 +0100

    ammend unit 2 page

--- src/components/UnitPages/unit2.jsx
+++ src/components/UnitPages/unit2.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../NavBar';
 
+//https://www.svgrepo.com/show/420911/computer-hardware-internet.svg
+
 const ProjectPage = () => {
   const [darkMode, setDarkMode] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
@@ -16,15 +18,13 @@ const ProjectPage = () => {
       <Navbar darkMode={darkMode} />
 
       {/* Centered Card */}
-      <div className="flex-grow flex flex-col justify-center items-center mt-20">
+      <div className="flex-grow flex flex-col justify-center items-center mt-16">
         <div className="max-w-md mx-auto bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-          <img className="w-full mb-0" src="https://www.svgrepo.com/show/420911/computer-hardware-internet.svg" alt="Landscape" />
+          <img className="w-full" src="https://www.svgrepo.com/show/420911/computer-hardware-internet.svg" alt="Landscape" />
           <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Technology Systems</div>
+            <div className="font-bold text-xl text-white mb-2">Technology Systems</div>
             <p className="text-base text-gray-300">
-              This is an exam unit on technology systems. In this unit, we learn how components of a technology system work together as well as understand different types of software.
-              Additionally, we learn how data flows between the internal components of a computer system and how it is processed to provide information.
-            </p>
+            In this tech systems exam, we study component interactions, software types, and data processing in computer systems.            </p>
           </div>
         </div>
       </div>
